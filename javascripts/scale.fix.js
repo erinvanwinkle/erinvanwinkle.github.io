0847740e060a1f170e4c8af0065b5e7231842735
erinvanwinkle <erin@erinvanwinkle.com>
2014-10-03 09:04:36 -0400

    js reload test

--- javascripts/scale.fix.js
+++ javascripts/scale.fix.js
@@ -1,6 +1,11 @@
 var metas = document.getElementsByTagName('meta');
 var i;
-location.reload()
+window.onload = function() {
+	if(!window.location.hash) {
+		window.location = window.location + '#loaded';
+		window.location.reload();
+	}
+}
 if (navigator.userAgent.match(/iPhone/i)) {
   for (i=0; i<metas.length; i++) {
     if (metas[i].name == "viewport") {
